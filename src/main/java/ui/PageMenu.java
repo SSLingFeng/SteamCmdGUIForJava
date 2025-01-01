1e6ee1489e510a234a925c803ce69c18e64c3071
czwMainEngine <582390473@qq.com>
2025-01-01 17:16:28 +0800

    增加基础配置类，读取写入config文件

--- src/main/java/ui/PageMenu.java
+++ src/main/java/ui/PageMenu.java
@@ -1,14 +1,4 @@
 package ui;
 
-import javax.swing.*;
-
 public class PageMenu {
-    private JPanel mainPanel;
-    private JPanel topPanel;
-    private JPanel leftPanel;
-    private JPanel centerPanel;
-    private JPanel rightPaenl;
-    private JPanel bottomPanel3;
-    private JTextField textField1;
-    private JButton readFile;
 }
